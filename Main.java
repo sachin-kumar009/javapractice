f25fb3e7ef63dfe92b5f6b7c4eb9c23249b4dbb3
Sachin kumar <singhsachin54896@gmail.com>
2023-09-30 23:46:40 +0530

    Add files via upload
    files uploaded

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        // write tour code here
+        System.out.print("The sum of three numbers is : ");
+        int num1 = 8;
+        int num2 = 9;
+        int num3 = 89;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+    }
+}
+// add two numbers
+// AddTwoNumbers --> pascal naming convention
+// addTwoNumbers --> camel case naming convention
+// --> this is used to comment out the programme
---
+++ Sachin_01.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Scanner;
+public class Sachin_01 {
+    public static void main(String[] args) {
+        System.out.println("taking Input from the user ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number 1 : ");
+        int a = sc.nextInt();
+        System.out.println("Enter a number 2 : ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("The sum of these number is :");
+        System.out.println(sum);
+        System.out.println(8*9);
+    }
+}
+// code is written for understanding the topic literals in java
+// byte  age =  67;
+// int age2 = 89;
+// short age3 = 90;
+// long ageDino = 688899;
+// char ch = 's';
+// float f1 = 5.6f;
+// double d1 = 4.66d;
+// boolean a = true;
+// System.out.println(age);
+// String str = "sachin kumar";
+// System.out.print(str);
+// System.out.print(a);
+// System.out.print(d1);
+// System.out.print(age3);
+// System.out.print(ageDino);
+// System.out.print(ch);
+// System.out.print(f1);
